a70f8c4a271fe42260168eabe32a64956d9cff20
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-09-06 02:49:50 +0600

    Add type annotations for person object and initialize with sample data

--- src/1.2.ts
+++ src/1.2.ts
@@ -2,3 +2,23 @@
 const firstName: string = "Parvez";
 const lastName: string = "Prottoy";
 console.log(`I am ${firstName} ${lastName}`);
+/* Type Annotation With Object */
+let person: {
+  name: string;
+  skill: string[];
+  gitHub: string;
+  portfolio?: string;
+  address: {
+    location: string;
+    phoneNumber?: number;
+  };
+};
+
+person = {
+  name: "Parvez",
+  skill: ["HTML", "CSS", "JS"],
+  gitHub: "",
+  address: {
+    location: "",
+  },
+};
